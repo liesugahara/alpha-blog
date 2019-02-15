256c5b2be8a19b7b8e45b9922eac5ee064589742
Kubera <kmorelo@opensky.com.co>
2019-02-15 08:33:21 -0500

    Add create categories test

---
+++ test/integration/create_categories_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+    test "get new category form and create category" do
+        get new_category_path
+    end
+end
